df70abf2f32ab232ab5dcf789bb9b79d20f25d61
noarkhh <jakub.pryc@gmail.com>
2025-03-21 11:32:33 +0100

    Finish lab2

--- lab_files/lab2/naive_bayes.py
+++ lab_files/lab2/naive_bayes.py
@@ -38,11 +38,23 @@ class NaiveBayes():
         :return: None
         """
         # 1. Count number of ham and spam emails
-        # 2. Reinitialize word count dictionaries
-        # 3. Process each email in the training set
-        raise NotImplementedError("Fit method not implemented") # Delete this line when you implement the method
+        self.num_train_hams = train_df[train_df.label == self.HAM_LABEL].shape[0]
+        self.num_train_spams = train_df[train_df.label == self.SPAM_LABEL].shape[0]
 
-    # TODO implement:
+        # 2. Reinitialize word count dictionaries
+        self.word_counts_ham = {}
+        self.word_counts_spam = {}
+
+        # 3. Process each email in the training set
+        for _, row in train_df.iterrows():
+            word_set = self.get_word_set(row["email"])
+            word_counts = self.word_counts_spam if row["label"] == self.SPAM_LABEL else self.word_counts_ham
+            for word in word_set:
+                if word not in word_counts:
+                    word_counts[word] = 1
+                else:
+                    word_counts[word] += 1
+
     def predict(self, email_text: str) -> str:
         """
         Predicts whether a single email is ham or spam.
@@ -51,11 +63,29 @@ class NaiveBayes():
         :return: The predicted label ('ham' or 'spam').
         """
         # 1. Get words in the email
+        word_set = self.get_word_set(email_text)
         # 2. Calculate prior probabilities
+        ham_prior_probability = self.num_train_hams / (self.num_train_hams + self.num_train_spams)
+        spam_prior_probability= self.num_train_spams / (self.num_train_hams + self.num_train_spams)
+
+
         # 3. Calculate log probabilities for ham and spam (for computational reasons)
+        ham_word_probabilities = {
+            word: np.log(self.word_counts_ham[word] / self.num_train_hams) for word in word_set if word in self.word_counts_ham
+        }
+        spam_word_probabilities = {
+            word: np.log(self.word_counts_spam[word] / self.num_train_spams) for word in word_set if word in self.word_counts_spam
+        }
         # 4. For each word in the email, update the log probabilities
+        ham_log_probabilities_sum = sum(ham_word_probabilities.values())
+        spam_log_probabilities_sum = sum(spam_word_probabilities.values())
         # 4. Predict label (in case of a tie, return HAM_LABEL)
-        raise NotImplementedError("Fit method not implemented") # Delete this line when you implement the method
+        ham_posterior_probability = ham_prior_probability * ham_log_probabilities_sum
+        spam_posterior_probability = spam_prior_probability * spam_log_probabilities_sum
+        if spam_posterior_probability > ham_posterior_probability:
+            return self.SPAM_LABEL
+        else:
+            return self.HAM_LABEL
     
     def predict_df(self, df: pd.DataFrame) -> pd.Series:
         """
@@ -85,3 +115,10 @@ class NaiveBayes():
                 correct += 1
                 
         return correct / total
+
+# train_df = pd.read_csv("train_emails.csv")
+# test_df = pd.read_csv("test_emails.csv")
+
+# naive_bayes = NaiveBayes()
+# naive_bayes.fit(train_df)
+# print(naive_bayes.accuracy(test_df))
